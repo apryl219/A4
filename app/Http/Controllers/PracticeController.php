93975bf63b16fbec19e2f7741888c5e573fe8f8c
apryl219 <apryl219@gmail.com>
2017-05-01 21:45:42 -0700

    packages and environment

--- app/Http/Controllers/PracticeController.php
+++ app/Http/Controllers/PracticeController.php
@@ -6,5 +6,20 @@ use Illuminate\Http\Request;
 
 class PracticeController extends Controller
 {
-    //
+
+	public function practice2() {
+		dump(config('app'));
+	}
+   public function practice1() {
+   		dump('This is the first example.');
+   }
+
+   public function index($n) {
+   		$method = 'practice'.$n;
+
+   		if(method_exists($this, $method))
+   			return $this->$method();
+   		else
+   			dd("Practice round [{$n}] not defined");
+   }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,22 @@
 <?php
 
-
+/**
+* Movie Related Routes
+*/
 Route::get('/movies', 'MovieController@index');
 
 Route::get('/movies/{title?}', 'MovieController@view');
 
+if(config('app.env') == 'local') {
+	#Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
+}
+/**
+*Practice
+*/
+Route::any('/practice/{n?}', 'PracticeController@index');
 
+/**
+*Main homepage visitors see when they visit just /
+*/
 Route::get('/', 'WelcomeController');
+
